d8cda9e6aeba49e1c2eb44cd8f6a2c1ef21c6e52
kumardeepak07 <39013184+kumardeepak07@users.noreply.github.com>
2025-07-30 19:47:23 +0530

    Delete

--- frontend/nestora/src/utils/Jwt.js
+++
@@ -1,7 +0,0 @@
-export const getToken = () => localStorage.getItem("token");
-
-export const isLoggedIn = () => !!getToken();
-
-export const logout = () => {
-  localStorage.removeItem("token");
-};
